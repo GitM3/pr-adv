197395d3e651de7181d8189c8e2955a44093c3dd
GitM3 <zander@polsons.info>
2026-01-06 20:14:06 +0900

    Dataset loading in tensor

--- load.py
+++ load.py
@@ -3,32 +3,81 @@ import matplotlib.pyplot as plt
 from phenobench.visualization import draw_semantics
 import numpy as np
 import os
-
+import tensorflow as tf
 from pprint import pprint
 
+DATA_DIR = os.path.expanduser("~/Development/08_ADV/PhenoBench")
 
-train_data = PhenoBench("~/Development/08_ADV/PhenoBench", 
-                        target_types=["semantics"])
+def _iter_samples(data, image_key="image", mask_key="semantics"):
+    for idx in range(len(data)):
+        sample = data[idx]
+        image = np.asarray(sample[image_key], dtype=np.uint8)
+        mask = np.asarray(sample[mask_key], dtype=np.uint16)
+        if mask.ndim == 3 and mask.shape[-1] == 1:
+            mask = mask[..., 0]
+        yield image, mask
+
+def _preprocess_sample(image, mask, image_size):
+    image = tf.ensure_shape(image, (None, None, 3))
+    mask = tf.ensure_shape(mask, (None, None))
+    mask = tf.expand_dims(mask, axis=-1)
+    image = tf.image.resize(image, image_size, method="bilinear")
+    mask = tf.image.resize(
+        mask, image_size, method=tf.image.ResizeMethod.NEAREST_NEIGHBOR
+    )
+    image = tf.cast(image, tf.float32) / 255.0
+    mask = tf.cast(mask, tf.int32)
+    return image, mask
 
 
-print(
-    f"PhenoBench ({train_data.split} split) contains {len(train_data)} images. We loaded the following targets: {train_data.target_types}."
-)
-print("The first entry contains the following fields:")
-pprint([f"{k} -> {type(v)}" for k, v in train_data[0].items()])
+def create_dataset(
+    root_dir,
+    split="train",
+    mask_key="semantics",
+    image_size=(128, 128),
+    shuffle=True,
+    seed=42,
+):
+    data = PhenoBench(root_dir, split=split, target_types=[mask_key])
+    output_signature = (
+        tf.TensorSpec(shape=(None, None, 3), dtype=tf.uint8),
+        tf.TensorSpec(shape=(None, None), dtype=tf.uint16),
+    )
+    dataset = tf.data.Dataset.from_generator(
+        lambda: _iter_samples(data, "image", mask_key),
+        output_signature=output_signature,
+    )
+    if shuffle:
+        dataset = dataset.shuffle(
+            buffer_size=min(1000, len(data)),
+            seed=seed,
+            reshuffle_each_iteration=True,
+        )
+    dataset = dataset.map(
+        lambda image, mask: _preprocess_sample(image, mask, image_size),
+        num_parallel_calls=tf.data.AUTOTUNE,
+    )
+    return dataset
 
-n_samples = 4
-n_rows = 4
-fig, axes = plt.subplots(ncols=n_samples, nrows=n_rows, figsize=(3 * n_samples, 3 * n_rows))
 
-indexes = np.random.choice(len(train_data), n_samples)
+def display_training_sample(dataset):
+    for image, mask in dataset.take(1):
+        if image.shape.rank == 4:
+            image = image[0]
+            mask = mask[0]
 
-for i in range(n_rows):
-    for j in range(n_samples):
-        axes[i, j].set_axis_off()
+        plt.figure(figsize=(8, 4))
+        plt.subplot(1, 2, 1)
+        plt.title("Input Image")
+        plt.imshow(tf.keras.utils.array_to_img(image))
+        plt.axis("off")
 
-for id, idx in enumerate(indexes):
-    axes[0, id].set_title(os.path.splitext(train_data[idx]["image_name"])[0])
-    draw_semantics(axes[0, id], train_data[idx]["image"], train_data[idx]["semantics"], alpha=0.5)
+        plt.subplot(1, 2, 2)
+        plt.title("True Mask")
+        plt.imshow(tf.squeeze(mask), cmap="viridis", vmin=0)
+        plt.axis("off")
+        plt.show()
 
-plt.show()
+
+dataset =  create_dataset(DATA_DIR)
+display_training_sample(dataset)
